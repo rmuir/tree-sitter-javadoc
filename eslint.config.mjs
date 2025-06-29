45878df3c0b022050f678a14d8553d5f61f0b599
Robert Muir <rmuir@apache.org>
2025-06-29 00:56:33 -0400

    style: remove useless escapes and crank up linter more

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,25 @@
+import eslint from '@eslint/js';
 import treesitter from 'eslint-config-treesitter';
 
 export default [
+  eslint.configs.recommended,
   ...treesitter,
+  {
+    files: ['grammar.js'],
+    languageOptions: {
+      globals: {
+        'alias': 'readonly',
+        'choice': 'readonly',
+        'field': 'readonly',
+        'grammar': 'readonly',
+        'optional': 'readonly',
+        'prec': 'readonly',
+        'repeat': 'readonly',
+        'repeat1': 'readonly',
+        'reserved': 'readonly',
+        'seq': 'readonly',
+        'token': 'readonly',
+      },
+    },
+  },
 ];
